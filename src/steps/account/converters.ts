1adea8f19a1445beaaa276b21671dafce0ce4482
Adam Pierson <adam.pierson@jupiterone.com>
2022-01-20 13:18:31 -0600

    Adding TODO for account value that was currently unavailable.

--- src/steps/account/converters.ts
+++ src/steps/account/converters.ts
@@ -9,6 +9,13 @@ export function getAccountKey(id: string): string {
   return `zoom_account:${id}`;
 }
 
+// TODO We're using the data available in api/v2/users/me to create the account
+// because the Account name is only available via api/v2/accounts/me or calling
+// it directly at api/v2/accounts/<accountID> and both options reside in the 
+// 'Master Account API' area and require :account:master OAuth token permissions.
+// If this becomes available without elevated permissions in the future, we
+// should change the account name below to be set to the name instead of the
+// account_number that we have right now.
 export function createAccountEntity(data: ZoomUser): Entity {
   return createIntegrationEntity({
     entityData: {
